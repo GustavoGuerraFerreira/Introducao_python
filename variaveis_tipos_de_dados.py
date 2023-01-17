a9ac429908e382c2c3403027ea59d92bbc85fa38
Gustavo <gutvasco123@gmail.com>
2023-01-17 12:25:37 -0300

    Primeiro commit

---
+++ variaveis_tipos_de_dados.py
@@ -0,0 +1,16 @@
+# Variáveis e tipos de dados
+
+# -> Receber dados
+# -> Armazenar dados
+# -> Processar dados
+# -> Mostrar dados processados
+
+# -> int (inteiro) -10 -100000 1000 10 0 
+# -> float (decimal) -10.1 -10.14324 10.8 20.65 1000.23
+# -> str(string) cadeia de caracteres "Caio Sampaino" 'Marcos' '0'
+# -> bool (Boleano) True False
+
+# -> float ()
+idade = 43
+idade = 50
+print(idade)
