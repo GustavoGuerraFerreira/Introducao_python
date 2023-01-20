b6e99becce8a8cb19cd3dee01fcafe886029994a
Gustavo <gutvasco123@gmail.com>
2023-01-20 09:30:40 -0300

    Operadore Relacionais e Lógicos

---
+++ 6_operadores_relacional.py
@@ -0,0 +1,17 @@
+# > < == !=
+operador_relacional =  2 > 3
+
+operador_relacional_maior =  5 > 2 
+operador_relacional_menor =  7 < 4  
+operador_relacional_maior_igual =  8 >= 8
+operador_relacional_menor_igual =  9 <= 7 
+operador_relacional_igual =  5 == 5 
+operador_relacional_diferente =  4 != 4 
+
+print(operador_relacional)  #False
+print(operador_relacional_maior) #True
+print(operador_relacional_menor) #False
+print(operador_relacional_maior_igual) #True
+print(operador_relacional_menor_igual) #False
+print(operador_relacional_igual) #True
+print(operador_relacional_diferente) #False
---
+++ 7_operadores_logico.py
@@ -0,0 +1,13 @@
+# and or not
+
+operadores_logico_and = 2 == 2 and 3 > 1 # True
+operadores_logico_or = 34 != 2 or 3>=2 # True
+operadores_logico_not = not(5 > 2) #False
+operadores_logico_and_not = not(operadores_logico_and) # False
+operadores_logico_or_not = not(operadores_logico_or) # False
+
+print(operadores_logico_and)
+print(operadores_logico_or)
+print(operadores_logico_not)
+print(operadores_logico_and_not)
+print(operadores_logico_or_not)
