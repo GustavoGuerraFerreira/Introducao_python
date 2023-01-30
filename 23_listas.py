0cd553558eb1595232da07e415c314a016dbb4e3
Gustavo <gutvasco123@gmail.com>
2023-01-30 09:35:44 -0300

    Listas

---
+++ 23_listas.py
@@ -0,0 +1,34 @@
+'''nomes  = ['João', 'Lucas', 'Matheus', 'Carlos']
+
+nomes[0] = "Gustavo"
+nomes.append('Estevão')
+print(nomes)'''
+''' 
+nomes = []
+while True:
+    nome = input("Digite N para sair, ou cadastre um nome: ")
+    if nome == "N":
+        break
+    nomes.append(nome)
+
+print(nomes)'''
+'''
+nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
+nomes.insert(1, 'Joao')
+print(nomes)
+'''
+
+'''
+nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
+
+nomes.pop(1)
+print(nomes)
+'''
+'''
+nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
+nomes.remove('Caio')
+print(nomes)
+'''
+nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
+print(nomes.index('Carlos'))
+print(nomes)
