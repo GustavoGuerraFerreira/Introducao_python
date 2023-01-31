e809fd99920ca849c0b06d31a99e7d07f24c1594
ggferreira <ggferreira@mpes.mp.br>
2023-01-31 16:07:07 -0300

    listas: ordenação/iteração

--- 23_listas.py
+++ 23_listas.py
@@ -29,6 +29,30 @@ nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
 nomes.remove('Caio')
 print(nomes)
 '''
+'''
 nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
 print(nomes.index('Carlos'))
 print(nomes)
+'''
+
+'''
+nomes = ['Caio', 'Marcos' , 'Carlos', 'Pedro']
+
+nomes_ordenado= sorted(nomes, reverse = True)
+
+print(nomes_ordenado)
+
+'''
+
+'''
+
+'''
+
+idades = [2, 3, 4,5,6,7,8,9,]
+
+idades_pares = []
+for i in idades:
+    if i % 2 == 0:
+        idades_pares.append(i)
+print(idades_pares)
+
